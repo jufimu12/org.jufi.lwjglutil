6dc0dbc6310a5bf8312fe1dcab32ec466cff28ca
jufimu12 <1Bombe2@web.de>
2014-08-29 17:13:06 +0100

    Added method to zip a directory

--- src/org/jufi/lwjglutil/FileUtils.java
+++ src/org/jufi/lwjglutil/FileUtils.java
@@ -3,13 +3,17 @@ package org.jufi.lwjglutil;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 public class FileUtils {
 	public static void extractZIP(File archive, File destDir) throws IOException {
@@ -56,6 +60,52 @@ public class FileUtils {
 		return new File(destDir, internalPathToEntry);
 	}
 	
+	public static void zipDirectory(File directory, String destination) throws FileNotFoundException, IOException {
+		ArrayList<File> fileList = new ArrayList<File>();
+		getAllFiles(directory, fileList);
+		writeZipFile(directory, fileList, destination);
+	}
+	private static void getAllFiles(File dir, ArrayList<File> fileList) throws IOException {
+		File[] files = dir.listFiles();
+		for (File file : files) {
+			fileList.add(file);
+			if (file.isDirectory()) getAllFiles(file, fileList);
+		}
+	}
+	
+	private static void writeZipFile(File directoryToZip, ArrayList<File> fileList, String zippath) throws FileNotFoundException, IOException {
+		FileOutputStream fos = new FileOutputStream(zippath);
+		ZipOutputStream zos = new ZipOutputStream(fos);
+		
+		for (File file : fileList) {
+			if (!file.isDirectory()) { // we only zip files, not directories
+				addToZip(directoryToZip, file, zos);
+			}
+		}
+		
+		zos.close();
+		fos.close();
+	}
+	
+	private static void addToZip(File directoryToZip, File file, ZipOutputStream zos) throws FileNotFoundException, IOException {
+		
+		FileInputStream fis = new FileInputStream(file);
+		
+		String zipFilePath = file.getCanonicalPath().substring(directoryToZip.getCanonicalPath().length() + 1,
+				file.getCanonicalPath().length());
+		ZipEntry zipEntry = new ZipEntry(zipFilePath);
+		zos.putNextEntry(zipEntry);
+		
+		byte[] bytes = new byte[1024];
+		int length;
+		while ((length = fis.read(bytes)) >= 0) {
+			zos.write(bytes, 0, length);
+		}
+		
+		zos.closeEntry();
+		fis.close();
+	}
+	
 	public static void copyJARRes(String source, String target, Class<?> executingClass) throws IOException {
 		InputStream stream = executingClass.getResourceAsStream(source);
 		if (stream == null) {
@@ -73,8 +123,6 @@ public class FileUtils {
 	}
 	public static void copyJARRess(String srcdir, String dstdir, Class<?> executingClass, String ... names) throws IOException {
 		for (String s : names) {
-			System.out.println(srcdir + s);
-			System.out.println(dstdir + s);
 			copyJARRes(srcdir + s, dstdir + s, executingClass);
 		}
 	}
